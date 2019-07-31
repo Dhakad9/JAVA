2cb72507cbe10eed69d11b39c0032a8b9c8f32d5
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2019-07-31 18:14:43 +0530

    updates

--- Java Programming/A.java
+++ Java Programming/A.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 class A
 {
-    int x=13;
-    public void display(){System.out.println("I am A");}
+    int x;
+    public A()
+    {
+        System.out.println("Constructor of class A");
+        x=13;
+    }
+    public A(int b)
+    {
+        x=b;
+    }
+    public void display()
+    {
+        System.out.println("I am A");
+    }
 }
---
+++ Java Programming/Honda.java
@@ -0,0 +1,12 @@
+class Honda extends Bike
+{
+    void run()
+    {
+        System.out.println("Running Safetly");
+    }
+    public static void main()
+    {
+        Bike ob=new Honda();
+        ob.run();
+    }
+}
