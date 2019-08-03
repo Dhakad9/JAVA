207d57d3348170f2800c0ca67978a316d51b330e
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2019-08-03 14:52:55 +0530

    packages

---
+++ Java Programming/Package_Same_Class.java
@@ -0,0 +1,12 @@
+class Package_Same_Class
+{
+    public static void main()
+    {
+        pack.abc ob1=new pack.abc();
+        mypack.abc ob2=new mypack.abc();
+        
+        ob1.display();
+        ob2.display();
+    }
+}
+
