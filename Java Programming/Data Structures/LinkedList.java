18b736b83ddbba0164f7248b6cde0bcfa6b90606
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2019-08-31 16:35:18 +0530

    Linked List

--- Java Programming/Data Structures/LinkedList.java
+++ Java Programming/Data Structures/LinkedList.java
@@ -12,22 +12,55 @@ class LinkedList
 			next=null;
 		}
 	}
-	public void printlist(){
-		Node n=head;
-		while(n!=null){
-			System.out.println(n.data+"\t");
-			n=n.next;
-		}
-	}
-	public void insertNode(int newData){
-		Node newNode=new Node(newData);
-		newNode.next=head;
-		head=newNode;
-	}
 	public boolean ifEmpty(LinkedList l){
 		if(l.head==null)
 			return true;
 		return false;
+	}
+	public void printlist(){
+		Node n=head;
+		while(n!=null){
+			System.out.print(n.data+"\t");
+			n=n.next;
+		}
+		System.out.println();
+	}
+	public void insertNodeAtFront(int newData){
+		Node newNode=new Node(newData);
+		newNode.next=head;
+		head=newNode;
+	}
+	public void insertNodeAtEnd(int newData){
+		Node newNode=new Node(newData);
+		if(head==null){
+				head=new Node(newData); 
+				return;
+		}
+
+		newNode.next=null;
+
+		Node last=head;
+		while(last.next!=null)
+			last=last.next;
+
+		last.next=newNode;
+	}
+	public void insertAtAnyPosition(int element,int pos){
+    	Node newNode = new Node(element);
+ 
+ 	   if (pos == 0){
+    	    newNode.next = head;
+        	head = newNode;
+    	}
+    	else
+    	{
+        	Node n = head;
+        	while (--pos>0){
+            	n = n.next;
+        	}
+        newNode.next = n.next;
+        n.next = newNode;
+    	}
 	}
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
@@ -37,19 +70,41 @@ class LinkedList
 
 			if(list.ifEmpty(list))
 				System.out.println("Node Empty");
+			else
+				System.out.println("Node have elements");
 
-		list.head=new Node(30);
-		Node second=new Node(20);
-		Node third=new Node(10);
+		list.head=new Node(40);
+		Node second=new Node(30);
+		Node third=new Node(20);
 
 		list.head.next=second;
 		second.next=third;
-
+		
+		System.out.println("Original Linked List");
 		list.printlist();
+		int element;
 		System.out.println("Enter a element to be inserted at front");
-		int element=in.nextInt();
-		list.insertNode(element);
+		element=in.nextInt();
+		list.insertNodeAtFront(element);
 		System.out.println("Node Inserted");
 		list.printlist();
+
+		System.out.println("Enter a element to be inserted at end");
+		element=in.nextInt();
+		list.insertNodeAtEnd(element);
+		System.out.println("Node Inserted");
+		list.printlist();
+
+		System.out.println("Enter a element and position where to be inserted");
+		element=in.nextInt();
+		int pos=in.nextInt();
+		list.insertAtAnyPosition(element,pos);
+		list.printlist();
+
+
+			if(list.ifEmpty(list))
+				System.out.println("Node Empty");
+			else
+				System.out.println("Node have elements");
 	}
 }
