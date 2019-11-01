6e95d4b91db2e1b0b65325e32959d33c36d49a2f
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2019-11-01 17:33:29 +0530

    Object File Handling

--- temp.java
+++ temp.java
@@ -3,19 +3,12 @@ import java.util.*;
 class temp{
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
-
-		System.out.println("Enter 2 string:");
+		System.out.println("Enter some values [char, String, String ]");
+		char a=in.nextLine().charAt(0);
+		System.out.println("[Character] You entered = "+a);
 		String s1=in.nextLine();
+		System.out.println("[String] You entered = "+s1);
 		String s2=in.nextLine();
-
-		System.out.println("String lenght="+s1.length());
-		System.out.println("String position="+s1.charAt(0));
-		System.out.println("String lenght="+s2.length());
-		System.out.println("String position="+s2.charAt(0));
-		System.out.println("s1 compare to s2="+s1.compareTo(s2));
-		System.out.println("Equal or not="+s1.equalsIgnoreCase(s2));
-		System.out.println("concatenation="+s1.concat(s2));
-		System.out.println("To Upper Case="+s1.toUpperCase());
-		System.out.println("To Lower Case="+s2.toLowerCase());
+		System.out.println("[String] You entered = "+s2);
 	}
 }
