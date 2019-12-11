2aeefe5a7c3259a1c6110c027e2824eaca643fa7
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2019-12-12 00:31:19 +0530

    Graph Data Structure

--- Java Programming/Data Structures/GraphDataStructure.java
+++ Java Programming/Data Structures/GraphDataStructure.java
@@ -29,6 +29,52 @@ class GraphDataStructure{
 			}
 		}
 	}
+	void DepthFirstSearch(int v, boolean visited[],Graph graph){
+		visited[v]=true;
+		System.out.print(v+"\t");
+
+		Iterator<Integer> i=graph.adjacentListArray[v].listIterator();
+		while(i.hasNext()){
+			int n=i.next();
+			if(!visited[n])
+				DepthFirstSearch(n,visited,graph);
+		}
+	}
+	void DFS(int v, Graph graph){
+		System.out.println("\nDepth First Search Transversl(from "+v+"): ");
+		boolean visited[]=new boolean[graph.vertices];
+
+		DepthFirstSearch(v,visited,graph);
+	}
+	void DFS(Graph graph){
+		//disconnected graph
+		//System.out.println("\nDepth First Search Transversl: ");
+		boolean visited[]=new boolean[graph.vertices];
+		for(int i=0;i<graph.vertices;i++){
+			if(visited[i]==false)
+				DepthFirstSearch(i,visited,graph);
+		}
+	}
+	void BFS(int s,Graph graph){
+		boolean visited[]=new boolean[graph.vertices];
+		LinkedList<Integer> queue=new LinkedList<Integer>();
+
+		visited[s]=true;
+		queue.add(s);
+
+		while(queue.size()!=0){
+			s=queue.poll();
+			System.out.print(s+"\t");
+			Iterator<Integer> i=graph.adjacentListArray[s].listIterator();
+			while(i.hasNext()){
+				int n=i.next();
+				if(!visited[n]){
+					visited[n]=true;
+					queue.add(n);
+				}
+			}
+		}	
+	}
 	public static void main(String[] args) {
 		Scanner in =new Scanner(System.in);
 
@@ -41,8 +87,13 @@ class GraphDataStructure{
 		while(!in.hasNext("-1")){
 			ob.addEdge(g,in.nextInt(),in.nextInt());
 		}
+		in.nextInt();
 		ob.printGraph(g);
-
-
+		System.out.println("\nEnter the starting point of DFS Transversl");
+		ob.DFS(in.nextInt(),g);
+		System.out.println("\nDFS Transversl for disconnected graph");
+		ob.DFS(g);
+		System.out.println("\nEnter the starting point of BFS Transversl");
+		ob.BFS(in.nextInt(),g);
 	}
 }
